fb4171859b6020aa34cb57fc3568e4f8e71eb6b6
rodrigoleguizamon <rodrigoleguizamon09@gmail.com>
2020-05-19 17:39:25 -0300

    Se agrega archivo sql

---
+++ db_playstore.sql
@@ -0,0 +1,48 @@
+create database if not exists db_playstore;
+
+create table users(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+	name varchar(95) NOT NULL,
+	email varchar(95) NOT NULL,
+	password varchar(95) NOT NULL,
+	created_at timestamp NULL DEFAULT NULL,
+	updated_at timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+);
+create table categories(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+    name varchar(95) NOT NULL,
+    PRIMARY KEY (id)
+);
+create table applications(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+    name varchar(95) NOT NULL,
+	image_url text,
+	description varchar(255) DEFAULT NULL, 
+	user_id bigint(20) NOT NULL, 
+	category_id bigint(20) NOT NULL, 
+	price decimal(5,2) NOT NULL,
+	created_at timestamp NULL DEFAULT NULL,
+	updated_at timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+);
+
+create table orders(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+    user_id bigint(20) NOT NULL, 
+    application_id bigint(20) NOT NULL,
+    price decimal(5,2) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create table comments(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+	order_id bigint(20) NOT NULL,
+	rating tinyint(5),
+	content varchar(255) DEFAULT NULL,
+	created_at timestamp NULL DEFAULT NULL,
+	updated_at timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+);
+
+
