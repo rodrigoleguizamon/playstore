757ac2327a31486f8f4917faa1493f88e3217c79
rodrigoleguizamon <rodrigoleguizamon09@gmail.com>
2020-05-26 21:03:56 -0300

    Se crean rutas y vistas para login y register

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var usersControllers= require ('../controllers/usersControllers');
+var {check, validationResult, body} = require('express-validator');
+const db = require('../database/models');
+
+/* GET register */
+router.get('/register', usersControllers.index)
+
+/* POST register */
+router.post('/register',[
+    check('name').isLength({min:1, max:30})
+    .withMessage('Ingrese su nombre'),
+
+    check('email').isEmail()
+    .withMessage('El campo debe ser un email valido'),
+    
+    check('password').isLength({min:6, max:12})
+    .withMessage('El password debe tener entre 6 y 12 caracteres'),
+], usersControllers.crear)
+
+/* GET login. */
+router.get('/login', usersControllers.login)
+
+/* POST login*/
+router.post('/login',[
+    check('email').isEmail()
+    .withMessage('El campo debe ser un email valido'),
+    
+    check('password').isLength({min:6, max:12})
+    .withMessage('El password debe tener entre 6 y 12 caracteres'),
+
+], usersControllers.ingresar) 
+
+
+
+module.exports = router;
