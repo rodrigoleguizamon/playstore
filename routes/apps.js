c5640a7d2838e500413bfa8d316f144ad26cefe9
rodrigoleguizamon <rodrigoleguizamon09@gmail.com>
2020-05-28 21:06:49 -0300

    se crea vista dinamica para administrar apps

--- routes/apps.js
+++ routes/apps.js
@@ -8,6 +8,8 @@ var appsControllers = require("../controllers/appsControllers")
 router.get('/admin', appsControllers.admin);
 router.get('/create', appsControllers.create);
 router.post('/create', appsControllers.guardar)
+router.get('/editar/:id',appsControllers.edit)
+router.post('/editar/:id',appsControllers.update)
 
 module.exports = router;
 
