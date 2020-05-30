44df1d265683bd0b63e79e17ae2f2b47a855a1ba
rodrigoleguizamon <rodrigoleguizamon09@gmail.com>
2020-05-29 21:16:03 -0300

    se agregan nuevas rutas

--- routes/apps.js
+++ routes/apps.js
@@ -8,8 +8,14 @@ var appsControllers = require("../controllers/appsControllers")
 router.get('/admin', appsControllers.admin);
 router.get('/create', appsControllers.create);
 router.post('/create', appsControllers.guardar)
-router.get('/editar/:id',appsControllers.edit)
-router.post('/editar/:id',appsControllers.update)
+router.get('/edit/:id',appsControllers.edit)
+router.post('/edit/:id',appsControllers.update)
+router.post('/admin/:id',appsControllers.delete)
+router.get('/detail/:id',appsControllers.detail)
+router.get('/order/:id',appsControllers.order)
+router.get('/myApps',appsControllers.appsList)
+
+
 
 module.exports = router;
 
