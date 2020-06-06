92dc0f1f4064878a5a2d549542e59290b325c97b
rodrigoleguizamon <rodrigoleguizamon09@gmail.com>
2020-06-06 18:55:29 -0300

    se crean rutas para buscador

--- routes/apps.js
+++ routes/apps.js
@@ -2,14 +2,28 @@
 var express = require('express');
 var router = express.Router();
 var appsControllers = require("../controllers/appsControllers")
+var multer = require('multer');
+var path = require('path')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+  var upload = multer({ storage: storage })
 
 
 
 router.get('/admin', appsControllers.admin);
 router.get('/create', appsControllers.create);
-router.post('/create', appsControllers.guardar)
+router.post('/create',upload.any(), appsControllers.guardar)
 router.get('/edit/:id',appsControllers.edit)
-router.post('/edit/:id',appsControllers.update)
+router.post('/edit/:id',upload.any(),appsControllers.update)
 router.post('/admin/:id',appsControllers.delete)
 router.get('/detail/:id',appsControllers.detail)
 router.post('/order/:id',appsControllers.order)
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var indexControllers = require("../controllers/indexControllers")
 
 /* GET home page. */
 router.get('/', indexControllers.listar);
+router.post('/search', indexControllers.search);
 router.get('/peliculas', indexControllers.listarPelis);
 router.get('/libros', indexControllers.listarLibros);
 router.get('/musica', indexControllers.listarMusica);
